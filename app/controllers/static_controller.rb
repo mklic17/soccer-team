0d149af3452e7e8067d4dfae0bc8979ad8f05cae
Mitchell Klich <MitchellKlich_2017@DePauw.edu>
2016-11-25 00:00:22 -0600

    added the sign-up page

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,25 +1,54 @@
 class StaticController < ApplicationController
+
   def main
-    a = Person.new('Mitchell Klich', 21, 'Chicago', 'Defense')
-    b = Person.new('Matthew Klich', 12, 'Chicago', 'Defense')
-    c = Person.new('Ruben Lugo', 23, 'Chicago', 'N/A')
-    d = Person.new('Roberto Sanchez', 03, 'Elmwood Park', 'Mid Field')
-    e = Person.new('Joe Romano', 07, 'Norridge', 'Forward')
-    @obj = [a, b, c, d, e]
+    @obj = makePeople();
   end
 
-  class Person
-    attr_accessor :name, :number, :from, :position
 
-    def initialize(name, number, from, position)
-      @name = name
-      @number = number
-      @from = from
-      @position = position
-    end
-  end
 
   def home
   end
 
+  def signUp
+    @obj = makeTeam();
+  end
+
+end
+
+
+
+class Person
+  attr_accessor :name, :number, :from, :position
+
+  def initialize(name, number, from, position)
+    @name = name
+    @number = number
+    @from = from
+    @position = position
+  end
+end
+
+def makePeople()
+  a = Person.new('Mitchell Klich', 21, 'Chicago', 'Defense')
+  b = Person.new('Matthew Klich', 12, 'Chicago', 'Defense')
+  c = Person.new('Ruben Lugo', 23, 'Chicago', 'N/A')
+  d = Person.new('Roberto Sanchez', 03, 'Elmwood Park', 'Mid Field')
+  e = Person.new('Joe Romano', 07, 'Norridge', 'Forward')
+  arr = [a, b, c, d, e]
+  return arr
+end
+
+class Team
+  attr_accessor :team
+  def initialize(team)
+    @team = team
+  end
+end
+
+def makeTeam()
+  a = Team.new('United States')
+  b = Team.new('Manchester')
+  c = Team.new('Chelsea')
+  arr = [a, b, c]
+  return arr
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   get 'home', to: 'static#home'
   get 'main', to: 'static#main'
-  
+  get 'teams', to: 'static#signUp'
+
   root 'static#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
